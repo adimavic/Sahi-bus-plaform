8fe9f2b9529838e1616617329e1c733700f36e67
Vishal Nehete <vishalnehete7870@gmail.com>
2025-09-25 11:00:00 +0000

    set favison to this : https://as1.ftcdn.net/v2/jpg/15/70/51/58/1000_F_15

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="https://as1.ftcdn.net/v2/jpg/15/70/51/58/1000_F_1570515886_DCbD02NHe0XW4s516DW8PBBnyuXPiBmM.jpg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
